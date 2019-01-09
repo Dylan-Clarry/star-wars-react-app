a4df53eeafd731f14a0eecb38d7068590a80bcef
Dylan Clarry <dylanclarry@Dylans-Air.lan>
2019-01-08 21:35:37 -0500

    Updates

--- precache-manifest.63a0d4c9f77edb7104685bad3b09cbb8.js
+++ precache-manifest.63a0d4c9f77edb7104685bad3b09cbb8.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/star-wars-react-app/static/js/runtime~main.6a595917.js"
   },
   {
-    "revision": "ff0c64bbbc23c3946e76",
-    "url": "/star-wars-react-app/static/js/main.ff0c64bb.chunk.js"
+    "revision": "aa37f43b4181b6a04d43",
+    "url": "/star-wars-react-app/static/js/main.aa37f43b.chunk.js"
   },
   {
     "revision": "4a34e45d34cafb7623bd",
     "url": "/star-wars-react-app/static/js/1.4a34e45d.chunk.js"
   },
   {
-    "revision": "ff0c64bbbc23c3946e76",
-    "url": "/star-wars-react-app/static/css/main.82f8fd46.chunk.css"
+    "revision": "aa37f43b4181b6a04d43",
+    "url": "/star-wars-react-app/static/css/main.9789bfa2.chunk.css"
   },
   {
-    "revision": "c9a0a1c829a3e1475d22730a77ca9f3a",
+    "revision": "1821675b7955ff054e7677a952c2727c",
     "url": "/star-wars-react-app/index.html"
   }
 ];
