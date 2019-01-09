3b745c6e4b7bcabd2b37cad04e724bce69cb5840
Dylan Clarry <dylanclarry@Dylans-Air.lan>
2019-01-08 21:21:06 -0500

    Updates

---
+++ precache-manifest.bb70c4318eda99adce4f592ddeaf8414.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "841c8f1a1cb913d25300",
+    "url": "/swapp/static/js/runtime~main.841c8f1a.js"
+  },
+  {
+    "revision": "dbae10769f0547135acc",
+    "url": "/swapp/static/js/main.dbae1076.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/swapp/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "dbae10769f0547135acc",
+    "url": "/swapp/static/css/main.9789bfa2.chunk.css"
+  },
+  {
+    "revision": "c6d601b01a31429d0667e554a07885da",
+    "url": "/swapp/index.html"
+  }
+];
